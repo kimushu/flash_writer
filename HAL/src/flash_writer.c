b1ce80701c104bf1717428bbeb5512ca6fddadd0
kimu_shu <alfvegardrisc@gmail.com>
2017-05-05 17:05:57 +0900

    Add spi support via alt_flash_dev

--- HAL/src/flash_writer.c
+++ HAL/src/flash_writer.c
@@ -12,7 +12,14 @@ int flash_writer_init(void)
 	if (result < 0) {
 		return result;
 	}
-#endif
+#endif  /* FLASH_WRITER_OCF_ENABLED */
+#ifdef FLASH_WRITER_SPI_ENABLED
+	extern int flash_writer_spi_init(const char *);
+	result = flash_writer_spi_init(root);
+	if (result < 0) {
+		return result;
+	}
+#endif  /* FLASH_WRITER_SPI_ENABLED */
 
 	return 0;
 }
--- flash_writer_sw.tcl
+++ flash_writer_sw.tcl
@@ -16,6 +16,7 @@ set_sw_property alt_sys_init_priority 1100
 add_sw_property c_source HAL/src/flash_writer.c
 add_sw_property c_source HAL/src/flash_file.c
 add_sw_property c_source HAL/src/flash_ocf.c
+add_sw_property c_source HAL/src/flash_spi.c
 
 add_sw_property include_source HAL/inc/flash_writer.h
 add_sw_property include_source HAL/inc/flash_file.h
@@ -30,5 +31,8 @@ add_sw_setting quoted_string system_h_define root.name FLASH_WRITER_ROOT_NAME "/
 add_sw_setting boolean_define_only system_h_define on_chip_flash.enabled FLASH_WRITER_OCF_ENABLED 0 "Enable altera_onchip_flash for MAX 10 devices"
 add_sw_setting quoted_string system_h_define on_chip_flash.name FLASH_WRITER_OCF_NAME "/dev/altera_onchip_flash_data" "Device name of altera_onchip_flash CSR"
 add_sw_setting boolean system_h_define on_chip_flash.read_only FLASH_WRITER_OCF_READ_ONLY 0 "Set read-only mode for altera_onchip_flash"
+add_sw_setting boolean_define_only system_h_define spi.enabled FLASH_WRITER_SPI_ENABLED 0 "Enable SPI flash device"
+add_sw_setting quoted_string system_h_define spi.name FLASH_WRITER_SPI_NAME "/dev/xxx" "Name of flash device"
+add_sw_setting boolean system_h_define spi.read_only FLASH_WRITER_SPI_READ_ONLY 0 "Set read-only mode for SPI flash access"
 
 # End of file
